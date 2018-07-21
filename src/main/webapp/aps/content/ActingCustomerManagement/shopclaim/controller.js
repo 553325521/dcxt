0bb7a550762498c9d5ba0c6114ee503b4745df8a
wangjinglong250 <1393936249@qq.com>
2018-07-21 16:15:36 +0800

    代理商文件夹创建

---
+++ src/main/webapp/aps/content/ActingCustomerManagement/shopclaim/controller.js
@@ -0,0 +1,15 @@
+
+	(function() {
+		define([], function() {
+			return [
+				'$scope', 'httpService', 'config', 'params', '$routeParams', 'eventBusService', 'controllerName', 'loggingService',
+				function($scope, $httpService, config, params, $routeParams, eventBusService, controllerName, loggingService) {
+					scope = $scope;
+					
+				}
+			];
+		});
+	}).call(this);
+
+	$(function() {
+	})
