127d753d702a43ae5656d84b2b92241fe3f0d19a
kqs <553325521@qq.com>
2018-07-22 11:18:11 +0800

    1.弹窗公共方法实现 2.标签增删改查初始化

--- src/main/webapp/aps/content/SystemSetup/BasicSetting/userTag/add/controller.js
+++ src/main/webapp/aps/content/SystemSetup/BasicSetting/userTag/add/controller.js
@@ -23,23 +23,47 @@
 					eventBusService.publish(controllerName, 'appPart.load.content', m2);
 				}
 
-				//保存事件
+				// 弹窗确认事件
 				eventBusService.subscribe(controllerName, controllerName + '.confirm', function(event, btn) {
+
+					$httpService.post(config.saveURL, $scope.form).success(function(data) {
+						if (data.code != '0000') {
+							var m2 = {
+								"title" : "提示",
+								"contentName" : "modal",
+								"text" : data.data,
+								"toUrl" : "aps/content/SystemSetup/BasicSetting/userTag/config.json?fid=" + scope.form.fid
+							}
+						} else {
+							var m2 = {
+								"title" : "提示",
+								"contentName" : "modal",
+								"text" : data.data,
+								"toUrl" : "aps/content/SystemSetup/BasicSetting/userTag/config.json?fid=" + scope.form.fid
+							}
+						}
+						eventBusService.publish(controllerName, 'appPart.load.modal', m2);
+					}).error(function(data) {
+						loggingService.info('获取测试信息出错');
+					});
+
+				});
+
+				// 弹窗取消事件
+				eventBusService.subscribe(controllerName, controllerName + '.close', function(event, btn) {
 					eventBusService.publish(controllerName, 'appPart.load.modal.close', {
 						contentName : "modal"
 					});
 				});
 
 				scope.doSave = function() {
-					$httpService.post(config.saveURL, $scope.form).success(function(data) {
-						if (data.code != '0000') {
-							loggingService.info(data.data);
-						} else {
-							$scope.$apply();
-						}
-					}).error(function(data) {
-						loggingService.info('获取测试信息出错');
-					});
+					var m2 = {
+						"url" : "aps/content/SystemSetup/BasicSetting/userTag/add/config.json",
+						"title" : "提示",
+						"contentName" : "modal",
+						"text" : "是否确定保存"
+					}
+					eventBusService.publish(controllerName, 'appPart.load.modal', m2);
 				}
 
 			}
