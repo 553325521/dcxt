199b88d7874a839330bf148612ad2af02fb28460
wangjinglong250 <1393936249@qq.com>
2018-07-23 19:31:15 +0800

    代理商界面修改

--- src/main/webapp/aps/content/ActingCustomerManagement/withdrawalManagement/presentRecord/controller.js
+++ src/main/webapp/aps/content/ActingCustomerManagement/withdrawalManagement/presentRecord/controller.js
@@ -37,6 +37,14 @@
 						money:'1200.00元',
 						state:'待结算'
 					}];
+					scope.toHref = function(path) {
+						var m2 = {
+							"url" : "aps/content/" + path + "/config.json?fid=" + params.fid,
+							"size" : "modal-lg",
+							"contentName" : "content"
+						}
+						eventBusService.publish(controllerName, 'appPart.load.content', m2);
+					}
 				}
 			];
 		});
