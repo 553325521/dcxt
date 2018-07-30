942fb8fd4601f0abb167674a6da9943b34478b06
wangjinglong250 <1393936249@qq.com>
2018-07-31 00:57:05 +0800

    界面修改

--- src/main/webapp/aps/content/ActingCustomerManagement/shopclaim/controller.js
+++ src/main/webapp/aps/content/ActingCustomerManagement/shopclaim/controller.js
@@ -6,6 +6,15 @@
 				function($scope, $httpService, config, params, $routeParams, eventBusService, controllerName, loggingService) {
 					scope = $scope;
 					scope.qrUrl = "aps/content/ActingCustomerManagement/shopclaim/code.png";
+					scope.pageTitle="商户认领";
+					scope.toHref = function(path) {
+						var m2 = {
+							"url" : "aps/content/" + path + "/config.json?fid=" + params.fid,
+							"size" : "modal-lg",
+							"contentName" : "content"
+						}
+						eventBusService.publish(controllerName, 'appPart.load.content', m2);
+					}
 				}
 			];
 		});
