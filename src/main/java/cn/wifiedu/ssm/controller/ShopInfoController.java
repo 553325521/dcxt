15d10e03ff44c58f9bfcc27e8ce558b00e02ed72
jhl <1104993914@qq.com>
2018-08-17 09:51:46 +0800

    优惠设置 新增功能

--- src/main/java/cn/wifiedu/ssm/controller/ShopInfoController.java
+++ src/main/java/cn/wifiedu/ssm/controller/ShopInfoController.java
@@ -1,5 +1,7 @@
 package cn.wifiedu.ssm.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,10 +14,12 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import cn.wifiedu.core.controller.BaseController;
 import cn.wifiedu.core.service.OpenService;
+import cn.wifiedu.core.vo.ExceptionVo;
 import cn.wifiedu.ssm.util.CookieUtils;
 import cn.wifiedu.ssm.util.redis.JedisClient;
 import cn.wifiedu.ssm.util.redis.RedisConstants;
@@ -41,6 +45,51 @@ public class ShopInfoController extends BaseController {
 	private JedisClient jedisClient;
 	
 	/**
+	 * 优惠买单设置
+	 * @throws Exception 
+	 */
+	@RequestMapping("/ShopInfo_saveYouhuimaidan_data")
+	public void saveYouhuimaidan() throws Exception {
+		Map<String, Object> map = getParameterMap();
+		
+		//String token = "o40NVwcZRjgFCE5GSb9JKb6luzb4";
+		String token = CookieUtils.getCookieValue(request, "DCXT_TOKEN");
+		String userJson = jedisClient.get(RedisConstants.REDIS_USER_SESSION_KEY + token);
+		JSONObject userObj = JSONObject.parseObject(userJson);
+		
+		map.put("SHOP_ID", userObj.get("FK_SHOP"));
+		map.put("USER_ID", userObj.get("USER_PK")); 
+		map.put("ROLE_ID", userObj.get("FK_ROLE")); 
+		
+		if(map.get("rule_model").toString().equals("1")) {
+			map.put("rule_model_first", map.get("yh_zkxf").toString());
+			map.put("rule_model_second", map.get("yh_zkyh").toString());
+		}else if(map.get("rule_model").toString().equals("2")) {
+			map.put("rule_model_first", map.get("yh_gdxf").toString());
+			map.put("rule_model_second", map.get("yh_gdj").toString());
+		}else if(map.get("rule_model").toString().equals("3")) {
+			map.put("rule_model_first", map.get("yh_sjxf").toString());
+			map.put("rule_model_second", map.get("yh_sjj").toString());
+		}
+		
+		map.put("sqlMapId", "saveYouhuimaidan");
+		String rulePk = openService.insert(map);
+		
+		map.put("fk_preferential_rule", rulePk);
+		
+		List<Map<String, Object>> list = toListMap(map.get("goodType").toString());
+		
+		for(int i=0; i<list.size(); i++) {
+			map.put("sqlMapId", "saveYouhuimaidanGood");
+			map.put("fk_goodtype", list.get(i).get("GTYPE_PK").toString());
+			openService.insert(map);
+		}
+		
+		output("success");
+	}
+	
+	
+	/**
 	 * 获取当前用户商铺信息
 	 * @param request
 	 * @param session
@@ -50,8 +99,8 @@ public class ShopInfoController extends BaseController {
 		try {
 			Map<String, Object> map = getParameterMap();
 
-			//String token = "o40NVwcZRjgFCE5GSb9JKb6luzb4";
-			String token = CookieUtils.getCookieValue(request, "DCXT_TOKEN");
+			String token = "o40NVwcZRjgFCE5GSb9JKb6luzb4";
+			//String token = CookieUtils.getCookieValue(request, "DCXT_TOKEN");
 			String userJson = jedisClient.get(RedisConstants.REDIS_USER_SESSION_KEY + token);
 			JSONObject userObj = JSONObject.parseObject(userJson);
 			
@@ -69,4 +118,18 @@ public class ShopInfoController extends BaseController {
 		}
 	}
 
+	
+	public static List<Map<String, Object>> toListMap(String json){
+    	List<Object> list =JSON.parseArray(json);
+    	
+    	List< Map<String,Object>> listw = new ArrayList<Map<String,Object>>();
+    	for (Object object : list){
+    	Map<String,Object> ageMap = new HashMap<String,Object>();
+    	Map <String,Object> ret = (Map<String, Object>) object;//取出list里面的值转为map
+    	
+    	listw.add(ret);
+    	}
+		return listw;
+    	
+    }
 }
--- src/main/webapp/aps/content/advnceSet/BasicSetting/favourablePay/baseAdd/controller.js
+++ src/main/webapp/aps/content/advnceSet/BasicSetting/favourablePay/baseAdd/controller.js
@@ -5,6 +5,63 @@
 			function($scope, $httpService, config, params, $routeParams, eventBusService, controllerName, loggingService) {
 				$scope.pageTitle = config.pageTitle;
 				$scope.form = {};
+				$scope.form.goodType = new Array();
+				
+				$scope.selectType = function(item){
+						var action = (item.checked ? 'add' : 'remove');
+						if (action == "add") {
+							$scope.form.goodType.push(item);
+							
+						} else {
+							$scope.form.goodType.remove(item);
+							
+						}
+				}
+				
+				$scope.good_scope = function(pa){
+					if(pa == "0"){
+						$(".goodType").hide();
+						$scope.form.goodType = $scope.goodType.slice(0);
+					}else{
+						$(".goodType").show();
+						$scope.form.goodType = [];
+					}
+				}
+				
+				$scope.doAdd = function(){
+					$scope.form.goodType = JSON.stringify($scope.form.goodType);
+					console.log($scope.form);
+					$httpService.post(config.saveYouhuimaidan, $scope.form).success(function(data) {
+						if (data.code === '0000') {
+							$scope.toHref("welcome");
+						} else {
+							$scope.toHref("welcome");
+						}
+						
+					}).error(function(data) {
+						loggingService.info('获取测试信息出错');
+					});
+				}
+				
+				$scope.isUser = function(pa){
+					$scope.form.is_use = pa;
+				}
+				
+				$scope.reset = function(){
+					$scope.form = {};
+				}
+				
+				$scope.remarkChange = function(){
+					$scope.remarkNum = $scope.form.remark.length;
+				}
+				
+				$scope.checkRuleModel = function(pa){
+					$(".rule_model_1").hide();
+					$(".rule_model_2").hide();
+					$(".rule_model_3").hide();
+					$(".rule_model_"+pa).show();
+					$scope.form.rule_model = pa;
+				}
 				
 				$scope.toHistory = function(){
 					var m2 = {
@@ -13,6 +70,25 @@
 							"contentName" : "content"
 						}
 					eventBusService.publish(controllerName, 'appPart.load.content', m2);
+				}
+				
+				var getGoodType = function(){
+					$scope.form.GTYPE_PID = "0";
+					$httpService.post(config.getGoodType, $scope.form).success(function(data) {
+						if (data.code === '0000') {
+							$scope.goodType = data.data;
+							for(var i=0; i<$scope.goodType.length; i++){
+								$scope.goodType[i].checked = false;
+							}
+							$scope.good_scope("0");
+							$scope.$apply();
+						} else {
+							
+						}
+						
+					}).error(function(data) {
+						loggingService.info('获取测试信息出错');
+					});
 				}
 				
 				var init = function(){
@@ -102,6 +178,11 @@
 					
 					$scope.form.SHOP_TYPE_2 = "请选择适用门店";
 					$scope.form.SHOP_TYPE_2_VALUE = '1';
+					$scope.form.good_scope = "0";
+					$scope.checkRuleModel('1');
+					
+					getGoodType();
+					
 				}
 				init();
 				
