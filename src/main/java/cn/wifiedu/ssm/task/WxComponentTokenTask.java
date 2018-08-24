1e18d26ee95cca3fe0793d09594580fdc794f7a3
kqs <553325521@qq.com>
2018-08-24 16:40:00 +0800

    提交修改

--- src/main/java/cn/wifiedu/ssm/task/WxComponentTokenTask.java
+++ src/main/java/cn/wifiedu/ssm/task/WxComponentTokenTask.java
@@ -2,11 +2,8 @@ package cn.wifiedu.ssm.task;
 
 import java.util.TimerTask;
 
-import javax.servlet.ServletContextEvent;
-
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import cn.wifiedu.ssm.controller.InterfaceController;
 import cn.wifiedu.ssm.util.StringDeal;
@@ -15,20 +12,19 @@ public class WxComponentTokenTask extends TimerTask {
 
 	private static Logger logger = Logger.getLogger(WxComponentTokenTask.class);
 	
-	private ServletContextEvent sce;
+	private ApplicationContext wac;
 	
 	/**
-	 * @param sce
+	 * @param wac
 	 */
-	public WxComponentTokenTask(ServletContextEvent sce) {
-		this.sce = sce;
+	public WxComponentTokenTask(ApplicationContext wac) {
+		this.wac = wac;
 	}
 
 	@Override
 	public void run() {
 		try {
 			// 每小时获取一次token保存到redis
-			ApplicationContext wac = WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext()); 
 			InterfaceController inter = (InterfaceController) wac.getBean("interfaceController");
 			String token = inter.getComponentToken();
 			logger.info("token:" + token + ", getTime:" + StringDeal.getStringDate());
--- src/main/java/cn/wifiedu/ssm/task/WxStartListener.java
+++ src/main/java/cn/wifiedu/ssm/task/WxStartListener.java
@@ -5,6 +5,9 @@ import java.util.Timer;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 public class WxStartListener implements ServletContextListener {
 
 	private Timer wxComponentTokenTimer;
@@ -20,12 +23,15 @@ public class WxStartListener implements ServletContextListener {
 	 * 监听开始执行
 	 */
 	public void contextInitialized(ServletContextEvent sce) {
+		
+		ApplicationContext wac = WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext()); 
+		
 		// 当监听开始执行时,设置一个TIME
 		Timer timer = new Timer();
 		WxStartTask task = new WxStartTask();
 		timer.schedule(task, 0, 3600 * 1000);
 		
 		wxComponentTokenTimer = new Timer();
-		timer.schedule(new WxComponentTokenTask(sce), 0, 3600 * 1000);
+		timer.schedule(new WxComponentTokenTask(wac), 0, 3600 * 1000);
 	}
 }
