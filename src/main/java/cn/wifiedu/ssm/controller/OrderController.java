257612f393850761fa3b5db6f32c28a30034e2f2
wangjinglong250 <1393936249@qq.com>
2018-10-17 13:53:30 +0800

    订单处理提交

--- src/main/java/cn/wifiedu/ssm/controller/OrderController.java
+++ src/main/java/cn/wifiedu/ssm/controller/OrderController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,6 +16,8 @@ import cn.wifiedu.core.service.OpenService;
 import cn.wifiedu.ssm.util.redis.JedisClient;
 import cn.wifiedu.ssm.util.redis.RedisConstants;
 
+@Controller
+@Scope("prototype")
 public class OrderController extends BaseController{
 
 	@Resource
@@ -30,6 +34,10 @@ public class OrderController extends BaseController{
 	public void setOpenService(OpenService openService) {
 	this.openService = openService;
 	}
+	/**
+	* <p>Title: loadOrderDataByTime</p>
+	* <p>Description: 根据时间、状态查询订单列表</p>
+	*/
 	@RequestMapping(value = "/Order_load_loadOrderDataByTime", method = RequestMethod.POST)
 	public void loadOrderDataByTime() {
 		try {
@@ -46,8 +54,34 @@ public class OrderController extends BaseController{
 					return;
 				}
 				map.put("sqlMapId","selectOrderByTime");
-				List<Map<String,Object>> cartDataList = openService.queryForList(map);
-				output("0000", cartDataList);
+				List<Map<String,Object>> orderDataList = openService.queryForList(map);
+				output("0000", orderDataList);
+			} else {
+				output("9999", "token无效");
+				return;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	* <p>Title: loadOrderDetailByOrderPK</p>
+	* <p>Description:根据订单id查询订单详情 </p>
+	*/
+	@RequestMapping(value = "/Order_load_loadOrderDetailByOrderPK", method = RequestMethod.POST)
+	public void loadOrderDetailByOrderPK() {
+		try {
+			Map<String, Object> map = getParameterMap();
+			String userJson = jedisClient.get(RedisConstants.REDIS_USER_SESSION_KEY + map.get("openid").toString());
+			if (StringUtils.isNotBlank(userJson)) {
+				if (!map.containsKey("ORDER_PK") || StringUtils.isBlank(map.get("ORDER_PK").toString())) {
+					output("9999", "订单ID无效");
+					return;
+				}
+				map.put("sqlMapId","selectOrderDetailByOrderPK");
+				List<Map<String,Object>> orderDataList = openService.queryForList(map);
+				output("0000", orderDataList);
 			} else {
 				output("9999", "token无效");
 				return;
